19db32826bdfebf11135a2e1247a0719fc890ea4
Natan Sales <victornatansalles@gmail.com>
2023-06-23 09:46:36 -0300

    calculadora react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
+  padding: 15px;
+  border: 1px solid #cdcdcd;
+
+  background-color: #373737;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 650;
+  flex: 1;
+
+  &:hover {
+    opacity: 0.4;
+  }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #aaffaa;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #aaffaa;
+    border: 0;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    padding: 0 15px;
+  }
+`
